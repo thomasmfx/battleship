a0867657c584bc37a09d69155170d66ee66a1e14
thomasmfx <thomasmoisesf@gmail.com>
2024-08-27 16:08:22 -0300

    build: Install html-loader

--- webpack.common.cjs
+++ webpack.common.cjs
@@ -23,7 +23,11 @@ module.exports = {
           {
             test: /\.(png|svg|jpg|jpeg|gif)$/i,
             type: 'asset/resource',
-          }
+          },
+          {
+            test: /\.html$/i,
+            loader: "html-loader",
+          }          
         ],
     },
 };

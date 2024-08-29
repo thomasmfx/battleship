aadd349f8b49ece7e3039426cd254653fb2ccd11
thomasmfx <thomasmoisesf@gmail.com>
2024-08-29 00:48:30 -0300

    refactor: Generate boards based on the Player class's board

--- src/dom/components/boards.js
+++ src/dom/components/boards.js
@@ -1,53 +1,56 @@
-import { newElementWithClass } from "../helpers.js"
-
-const boardPlayer1 = document.querySelector('.player-1');
-const boardPlayer2 = document.querySelector('.player-2');
-const positionsY = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+import { isShip } from "../../helpers/gameBoardHelpers.js";
+import { newElementWithClass } from "../helpers.js";
 
 function newSquare(x, y) {
-  const square = document.createElement('button')
-  square.classList.add('board__square', 'fa-solid', 'fa-water');
+  let square = document.createElement('button');
+  square.classList.add('button', 'board__square', 'fa-solid', 'fa-water');
   square.dataset.x = x;
   square.dataset.y = y;
+
   return square;
-}
+};
+
+function newShip(x, y) {
+  let ship = document.createElement('div');
+  ship.classList.add('ship-cover');
+  ship.dataset.x = x;
+  ship.dataset.y = y;
+
+  return ship;
+};
 
 function setGridArea(element, rowStart, colStart) {
   element.style.gridRow = `${rowStart} / span 1`;
   element.style.gridColumn = `${colStart} / span 1`;
 };
 
-function appendPositionsX(board) {
+export function appendPositionsLabels(board) {
+  const letterPositions = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+
   for (let i = 0; i < 10; i++) {
-    const element = newElementWithClass('p', positionsY[i], 'board__positions');
-    // row 1 is already of an empty div, gotta start from row 2
-    setGridArea(element, i + 2, 1);
-    board.appendChild(element);
+    const positionX = newElementWithClass('p', 'board__positions');
+    const positionY = newElementWithClass('p', 'board__positions');
+
+    positionX.textContent = i;
+    setGridArea(positionX, 1, i + 2);
+    positionY.textContent = letterPositions[i];
+    setGridArea(positionY, i + 2, 1);
+
+    board.append(positionX, positionY);
   };
+
 };
 
-function appendPositionsY(board) {
-  for (let i = 0; i < 10; i++) {
-    let element = newElementWithClass('p', i, 'board__positions');
-    // column 1 is already of an empty div, gotta start from column 2
-    setGridArea(element, 1, i + 2);
-    board.appendChild(element);
-  };
-};
+export function fillBoard(player, DOMboard) {
+  let playerBoard = player.board.grid;
 
-function fillBoardSquares(board) {
-  for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++) {
-      board.appendChild(newSquare(i, j))
+  for (let x = 0; x < playerBoard.length; x++) {
+    for (let y = 0; y < playerBoard[x].length; y++) {
+      let currPosition = player.board.getValueAt(x, y);
+
+      isShip(currPosition)
+      ? DOMboard.append(newShip(x, y))
+      : DOMboard.append(newSquare(x, y));
     };
   };
 };
-
-function loadBoard(board) {
-  appendPositionsY(board);
-  appendPositionsX(board);
-  fillBoardSquares(board);
-};
-
-loadBoard(boardPlayer1);
-loadBoard(boardPlayer2);

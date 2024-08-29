d800c2795308657715c46a34404ccc46745810f7
thomasmfx <thomasmoisesf@gmail.com>
2024-08-29 00:49:37 -0300

    feat: Add game controller to interact between the UI and logic

---
+++ src/controllers/game.js
@@ -0,0 +1,30 @@
+import { Player } from '../models/player.js';
+import { appendPositionsLabels, fillBoard } from '../dom/components/boards.js';
+
+let currentTurn;
+
+const player1 = new Player();
+const player2 = new Player(true);
+
+player1.board.placeShip('carrier', false, 2, 1);
+player1.board.placeShip('battleship', true, 0, 2);
+player1.board.placeShip('battleship', false, 0, 6);
+player1.board.placeShip('cruiser', true, 5, 5);
+player1.board.placeShip('cruiser', false, 5, 3);
+player1.board.placeShip('cruiser', false, 3, 9);
+player1.board.placeShip('cruiser', true, 3, 3);
+player1.board.placeShip('submarine', false, 9, 6);
+player1.board.placeShip('submarine', false, 9, 3);
+player1.board.placeShip('submarine', false, 9, 0);
+player1.board.placeShip('submarine', false, 9, 9);
+
+const player1Board = document.querySelector('.player-1');
+const player2Board = document.querySelector('.player-2');
+
+function loadBoard(player, DOMboard) {
+  appendPositionsLabels(DOMboard);
+  fillBoard(player, DOMboard);
+};
+
+loadBoard(player1, player1Board);
+loadBoard(player2, player2Board);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './styles/main.scss';
 import '../assets/waves.svg';
+
 import './dom/components/boards.js';
+
+import './controllers/game.js';

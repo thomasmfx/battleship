117ecc9a07f269660c306cae63db2b952b5eb6b8
thomasmfx <thomasmoisesf@gmail.com>
2024-08-29 00:43:22 -0300

    refactor: Remove parameter from helper function

--- src/dom/helpers.js
+++ src/dom/helpers.js
@@ -1,6 +1,5 @@
-export function newElementWithClass(el, text, ...classes) {
+export function newElementWithClass(el, ...classes) {
   const element = document.createElement(`${el}`);
-  element.textContent = text;
   for (const cl of classes) {
     element.classList.add(`${cl}`)
   };

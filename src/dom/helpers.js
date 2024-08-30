b3d435dae6b002151c3dc17a4e9ea7ab199e0ce1
thomasmfx <thomasmoisesf@gmail.com>
2024-08-30 12:41:55 -0300

    style: Rename helper function

--- src/dom/helpers.js
+++ src/dom/helpers.js
@@ -1,4 +1,4 @@
-export function newElementWithClass(el, ...classes) {
+export function createElementWithClass(el, ...classes) {
   const element = document.createElement(`${el}`);
   for (const cl of classes) {
     element.classList.add(`${cl}`)
